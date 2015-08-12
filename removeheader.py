7d2effbaca21fbcec75cddd77144b96cc0b17405
woook <dev@stickie.be>
2015-08-13 00:25:05 +0100

    pep8

--- removeheader.py
+++ removeheader.py
@@ -2,7 +2,7 @@ import sys
 #with open('NGS43a_H_141105A_ClinEx_1_1407200_135136_S_ATCACG_Exon_GeneLevelRDsummary' + '.csv') as coverageall:
 with open(sys.argv[1] + '.csv') as coverageall:
 	import csv
-	readfile = csv.reader(coverageall, delimiter = '\t')
+	readfile = csv.reader(coverageall, delimiter='\t')
 	line = 0
 	for row in readfile:
 		#create the output (will append if this exists already)
@@ -12,3 +12,4 @@ with open(sys.argv[1] + '.csv') as coverageall:
 				filteredfilewriter.writerow(row)                
 		#iterate through the loop
 		line += 1
+		
