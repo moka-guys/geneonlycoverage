e5a6bd0da74a5a6a54d4da1d2f097ff118af8bd1
woook <dev@stickie.be>
2015-08-13 00:07:29 +0100

    changed output directory

--- geneonlycoverage.py
+++ geneonlycoverage.py
@@ -10,15 +10,15 @@ with open(sys.argv[1] + '.csv') as coverageall:
 	readfile = csv.reader(coverageall, delimiter = '\t')
 	line = 0
 	for row in readfile:
-		#create the output (will overwrite if this exists already) and write the header row to it, adding sample field
+		#create the output (will overwrite if this exists already) in new folder (needs to exist in case of permissions) and write the header row to it, adding sample field
 		if line < 1 :
-			with open(sys.argv[2] + '.csv', mode='w') as coveragegenes:
+			with open('geneonly/' + sys.argv[2] + '.csv', mode='w') as coveragegenes:
 				filteredfilewriter = csv.writer(coveragegenes, delimiter='\t', lineterminator='\n')
 				row.append('Sample')
 				filteredfilewriter.writerow(row)          
 		#append only the gene coverage rows to the output
 		if line > 0 and row[4] == 'Whole gene':
-			with open(sys.argv[2] + '.csv', mode='a') as coveragegenes:
+			with open('geneonly/' + sys.argv[2] + '.csv', mode='a') as coveragegenes:
 				filteredfilewriter = csv.writer(coveragegenes, delimiter='\t', lineterminator='\n')
 				row.append(sys.argv[2])
 				filteredfilewriter.writerow(row)                
